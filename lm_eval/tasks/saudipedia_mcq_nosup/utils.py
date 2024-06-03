5c8d756d7ea5ef14b001cef7c245cd47df1705f4
Abdulhamed Alothaimen <aalothaimen@sdaia.gov.sa>
2024-06-03 11:05:13 +0300

    added saudipedia mcq no support

---
+++ lm_eval/tasks/saudipedia_mcq_nosup/utils.py
@@ -0,0 +1,16 @@
+import datasets
+
+subtasks = ['Islamic Studies', 'Science', 'Social', 'Biology', 'Physics']
+subtasks_ar = ['الدراسات الإسلامية', 'العلوم', 'الاجتماعيات', 'علم الأحياء', 'علم الفيزياء']
+
+def process_docs(dataset: datasets.Dataset) -> datasets.Dataset:
+    # def _process_doc(doc):
+    
+    #     subject = doc['id'].split("-")[0]
+    #     subject_ar = subtasks_ar[subtasks.index(subject)]
+    #     out_doc = {**doc, 'subject_ar': subject_ar}
+    #     print(subject_ar)
+    #     print(out_doc)
+    #     return out_doc
+
+    return dataset
